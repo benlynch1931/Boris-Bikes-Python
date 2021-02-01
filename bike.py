b190ddf63c8ee86456d7aaada95fde0df3c3a39a
Ben <ben.lynch1931@gmail.com>
2021-02-01 14:21:40 +0000

    Red/Green: bikes have names

--- bike.py
+++ bike.py
@@ -1,7 +1,11 @@
 class Bike:
 
-    def __init__(self):
+    def __init__(self, number):
         self.working = True
+        self.bike_name = 'Bike {0}'.format(number)
 
     def is_working(self):
         return self.working
+
+    def name(self):
+        return self.bike_name
--- test_bike.py
+++ test_bike.py
@@ -7,6 +7,10 @@ Bike = bike.Bike
 class TestBike(unittest.TestCase):
 
     def test_bike_working(self):
-        bike = Bike()
+        bike = Bike(1)
         self.assertIsInstance(bike, Bike)
         self.assertEqual(bike.is_working(), True)
+
+    def test_bike_name(self):
+        bike = Bike(1)
+        self.assertEqual(bike.name(), 'Bike 1')
