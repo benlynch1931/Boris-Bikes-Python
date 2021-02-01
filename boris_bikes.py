ea02ceb5f11b30d4aa597be359224a28d7b48d0c
Ben <ben.lynch1931@gmail.com>
2021-02-01 12:28:24 +0000

    Red/Green: DockingStation can be instantiated

---
+++ boris_bikes.py
@@ -0,0 +1,6 @@
+class DockingStation:
+    # This is a class for docking stations
+
+    def __init__(self):
+        # print("Initialize method")
+        return None
--- test_boris_bikes.py
+++ test_boris_bikes.py
@@ -1,6 +1,11 @@
 import unittest
 import boris_bikes
 
+DockingStation = boris_bikes.DockingStation
+
 class TestBorisBikes(unittest.TestCase):
 
-    def 
+    def test_docking_station_instance(self):
+        docking_station = DockingStation()
+        self.assertIsInstance(docking_station, DockingStation)
+    # def test_release_bike(self):
